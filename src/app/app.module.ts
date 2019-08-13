d040c9c4fddbcfbe73f87949ebeb579fb8378479
Chris Dinh <dinh.thanhtoan@gmail.com>
2019-08-13 23:19:46 +1200

    work-id-247: Added simple site header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { SharedHeaderComponent } from './components/shared/header/shared-header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      SharedHeaderComponent
+   ],
+   imports: [
+      RouterModule.forRoot([]),
+      BrowserModule
+   ],
+   providers: [],
+   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/shared/header/shared-header.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+   selector: 'shared-header',
+   templateUrl: './shared-header.component.html'
+})
+
+export class SharedHeaderComponent {
+
+   backToHome() {
+      console.log("We should redirect to the web console");
+      // this.router.navigateByUrl(`${environment.web_console}`);
+   }
+}
