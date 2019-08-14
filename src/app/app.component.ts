1c5f8f427dac61a482b1798f1db6a684d1b60e19
Chris Dinh <dinh.thanhtoan@gmail.com>
2019-08-14 20:14:18 +1200

    work-id-247: Add basic HomeComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
    selector: 'app-root',
    templateUrl: './app.component.html'
 })
+
 export class AppComponent {
-   title = 'MultilinksPortal';
+   
+   constructor(private router: Router) {}
+
+   ngOnInit() {
+      this.router.navigate(['home']);
+   }
 }
