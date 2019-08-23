6d131b367b92dbca3e02c2f6408f925f7a8cb764
Chris Dinh <dinh.thanhtoan@gmail.com>
2019-08-23 20:52:53 +1200

    work-id-260: Redirect to Identity Service on register/login click event

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MultilinksIdentityService } from 'src/app/services/multilinks-identity.service';
 
 @Component({
    selector: 'home',
@@ -7,9 +8,17 @@ import { Component } from '@angular/core';
 
 export class HomeComponent {
 
-   constructor() {}
+   constructor(private identityService: MultilinksIdentityService) {}
    
    ngOnInit() {
       particlesJS.load('particles', '../../../assets/js/particles.json', () => {})
+   }
+
+   register() {
+      this.identityService.registerUser();
+   }
+
+   signin() {
+      this.identityService.triggerSignIn();
    }
 }
