0fbaa8b27d8d7a048a1456574c7082d35d18b1f8
Chris Dinh <dinh.thanhtoan@gmail.com>
2019-08-27 22:55:41 +1200

    work-id-260: Always show header and footer

--- src/app/components/shared/header/shared-header.component.ts
+++ src/app/components/shared/header/shared-header.component.ts
@@ -1,6 +1,8 @@
 ﻿import { Component } from '@angular/core';
 import { AppEnvironment } from 'src/app/services/app-environment.service';
 import { MultilinksIdentityService } from 'src/app/services/multilinks-identity.service';
+import { MultilinksCoreService } from 'src/app/services/multilinks-core.service';
+import { timer } from 'rxjs';
 
 @Component({
    selector: 'shared-header',
@@ -10,9 +12,21 @@ import { MultilinksIdentityService } from 'src/app/services/multilinks-identity.
 export class SharedHeaderComponent {
 
    releaseInfo: string;
+   connectionActive: boolean;
    
    constructor(private appEnv: AppEnvironment,
-      public identityService: MultilinksIdentityService) {
+      public identityService: MultilinksIdentityService,
+      private coreService: MultilinksCoreService) {
       this.releaseInfo = this.appEnv.getReleaseInfo();
+   }
+
+   ngOninit() {
+      this.coreService.connectionActive$.subscribe((connectionActive) => {
+         if (connectionActive) {
+            const holdoffTimer = timer(3000).subscribe(() => {
+               this.connectionActive = true;
+            });
+         }
+      });
    }
 }
